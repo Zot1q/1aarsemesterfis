d477af0c8674019983b24947292608060e88657a
Christian Guldager <chri707i@student.eadania.dk>
2018-05-22 09:43:27 +0200

    Log

--- 1AarsProjekt/1AarsProjekt/Model/LOG/ErrorLog.cs
+++ 1AarsProjekt/1AarsProjekt/Model/LOG/ErrorLog.cs
@@ -8,5 +8,35 @@ namespace _1AarsProjekt.Model.LOG
 {
     class ErrorLog
     {
+        /// <summary>
+        /// ID for the database
+        /// </summary>
+        public int ErrorID { get; set; }
+        /// <summary>
+        /// Message for the log
+        /// </summary>
+        public string LogMessage { get; set; }
+        /// <summary>
+        /// ErrorNr represents system errors : 0 = no error, 1 = dataAccessLayer Error, 2 = User Error etc...
+        /// </summary>
+        public int ErrorNr { get; set; }
+
+        /// <summary>
+        /// The date and time of the error
+        /// </summary>
+        public string DateAndTime { get; set; }
+        /// <summary>
+        /// Amount of data inserted in the database
+        /// </summary>
+        public int AmountOfData { get; set; }
+
+        public ErrorLog(int errorNr, string logMessage, string dateAndTime, int amountOfData)
+        {
+            ErrorNr = errorNr;
+            LogMessage = logMessage;
+            DateAndTime = dateAndTime;
+            AmountOfData = amountOfData;
+
+        }
     }
 }
